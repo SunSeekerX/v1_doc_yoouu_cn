cbce33284a16cbff9ad9856f5439a587013b3423
SunSeekerX <1647800606@qq.com>
2020-04-12 23:10:24 +0800

    Init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+/**
+ * @name: 
+ * @author: SunSeekerX
+ * @Date: 2020-04-12 22:42:30
+ * @LastEditors: SunSeekerX
+ * @LastEditTime: 2020-04-12 22:48:05
+ */
+
+ module.exports = {
+  base: '/',
+  title: 'SunSeekerX',
+  description: 'This road is just beginning ~',
+  // favicon: 网站图标
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico'
+      }
+    ]
+  ],
+  themeConfig:{
+    repo: 'PanJiaChen/vue-element-admin',
+    docsRepo: 'PanJiaChen/vue-element-admin-site',
+  }
+ }
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroImage: /home.jpg
+heroText: SunSeekerX
+tagline: This road is just beginning ~
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+footer: MIT Licensed | Copyright © 2020-present SunSeekerX
+---
